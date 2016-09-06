c53e8b5dbed3d690162b0c0b6ecf2807cc76084f
iambrianbui <iambrianbui@yahoo.com>
2016-09-05 23:27:55 -0700

    Update ExtraCreditLab stack.h

--- ExtraCreditLab stack.h
+++ ExtraCreditLab stack.h
@@ -1,47 +1,55 @@
-#ifndef Stack
-#define Stack
+#ifndef Stack  //  if there is no Stack header
+#define Stack  //  define the Stack header
+  
+template <class TYPE, int INDEX>  //  template, class TYPE because we do not know the type, but we know the second value must be an integer
 
-template <class TYPE, int INDEX>
-
-class STACK{
-private:  
-	TYPE elt[INDEX];
-	int counter;
-
+class STACK{  //  BEGIN CLASS MAKING
+private:    //  don't touch 
+	TYPE elt[INDEX];  //  array of elements
+	int counter;  //  counter of "trays"
+	  
 public:
 
-	void clearStack();
-	bool emptyStack();
-	bool fullStack();
-	void pushStack(int x);
-	int popStack();
+	void clearStack();  //  works as a constructor that can be called repeatedly
+	bool emptyStack();  //  checks if the stack is empty
+	bool fullStack();  //  checks if the stack is full
+	void pushStack(int x);  //  insert trays
+	void pushStack(std::string x);  //  OVERLOAD WOAHHHHH for strings, I think this could be a template too, but not too sure....
+	TYPE popStack();  //  take out trays
 };
 
-template <class TYPE, int INDEX>
-void STACK<TYPE, INDEX>::clearStack(){
-	counter = 0;
+template <class TYPE, int INDEX>  //  gotta do this everytime, especially in the definitions
+void STACK<TYPE, INDEX>::clearStack(){  //  clear stack function
+	counter = 0;  //  set the counter to 0
 }
 
 template <class TYPE, int INDEX>
-bool STACK<TYPE, INDEX>::emptyStack(){
-	return (counter == 0) ? true : false;
+bool STACK<TYPE, INDEX>::emptyStack(){  //  empty stack function
+	return (counter == 0) ? true : false;  //  RETURN TRUE if counter == 0, else false
 }
 
 template <class TYPE, int INDEX>
-bool STACK<TYPE, INDEX>::fullStack(){
-	return (counter == 5) ? true : false;
+bool STACK<TYPE, INDEX>::fullStack(){  //  full stack function
+	return (counter == INDEX) ? true : false;  //  RETURN TRUE if counter == the index,  admittedly this was counter == 5 and I got really lucky changing it to index pHEW
 }
 
 template <class TYPE, int INDEX>
-void STACK<TYPE, INDEX>::pushStack(int x){
-	elt[counter] = x;
-	counter++;
+void STACK<TYPE, INDEX>::pushStack(int x){  //  push stack function for integers
+	elt[counter] = x;  //  have x be the tray
+	counter++;  //  increase the counter
 }
 
 template <class TYPE, int INDEX>
-int STACK<TYPE, INDEX>::popStack(){
-	counter--;
-	return elt[counter];
+void STACK<TYPE, INDEX>::pushStack(std::string x){  //  push stack function of strings
+	elt[counter] = x;  //  have x be the tray
+	counter++;  //  increase the counter
+}
+
+template <class TYPE, int INDEX>
+TYPE STACK<TYPE, INDEX>::popStack(){  //  pop stack function,  THIS HAS TO BE TYPE AND NOT INT!!!! because of THIS, i'm PRETTY SURE (?) I didn't have to overload, and I could have made main 6 lines (?), but this program does
+	//  just fine
+	counter--;  //  decrease the counter
+	return elt[counter];  //  return the value
 }
 
 #endif
