8cee6ba6789e6f43ee94e56f6ccbf6f229c5b9ac
iambrianbui <iambrianbui@yahoo.com>
2016-10-12 22:20:09 -0700

    Update STACK.h

--- STACK.h
+++ STACK.h
@@ -1,57 +1,58 @@
-#ifndef STACK
-#define STACK
+#ifndef STACK  //  if not defined
+#define STACK  //  define STACK
 
 #include <iostream>
 using namespace std;
 
 
-template <class T>
-class STACK_LIST{
-private:  struct node{
-	T info;
-	node *next;
+template <class T>  // template
+class STACK_LIST{  //  create class STACK_LIST
+private:  struct node{  //  node stuff
+	T info;  //  info here of type T
+	node *next;  //  bring me to the next room
 };
-		  node *stack;
-public:  STACK_LIST();
-		 void pushStack(T);
-		 bool emptyStack();
-		 T popStack();
+		  node *stack;  //  initialize the first node
+public:  STACK_LIST();  //  constructor sets stack to null
+		 void pushStack(T);  //  push T
+		 bool emptyStack();  //  check if stack is empty
+		 T popStack();  //  pop T
 };
 
 template<class T>
 STACK_LIST<T>::STACK_LIST(){
-	stack = NULL;
+	stack = NULL;  //  set to null
 }
 
 template<class T>
 void STACK_LIST<T>::pushStack(T x){
-	node *p;
-	p = new node;
-	p->info = x;
-	p->next = NULL;
-	if (stack == NULL){
-		stack = p;
+	node *p;  //  create node p
+	p = new node;  //  p is a new node, so find space for it
+	p->info = x;  //  the info inside p is x
+	p->next = NULL;  //  the new node is pointing to nothing
+	if (stack == NULL){  //  if stack is pointing to null
+		stack = p;  //  this node is the first node
 	}
-	else{
-		p->next = stack;
-		stack = p;
+	else{  //  else
+		p->next = stack;  //  the next node is pointing to stack
+		stack = p;  //  this is the new "first node"
 	}
 
 }
 
 template<class T>
 bool STACK_LIST<T>::emptyStack(){
-	if (stack == NULL) return true;
-	else return false;
+	if (stack == NULL) return true;  //  if stack is empty than true
+	else return false;  //  else false
 }
 
 template<class T>
 T STACK_LIST<T>::popStack(){
-	node *p = stack; T x;
-	x = stack->info;
-	stack = stack->next;
-	delete (p);
-	return x;
+	node *p = stack; //  create node p which is a copy of the master key
+	T x;  //  initialize x which is type T
+	x = stack->info;  //  x is the info of the front node
+	stack = stack->next;  //  change the front node
+	delete (p);  //  delete p
+	return x;  //  return x
 }
 
 #endif
